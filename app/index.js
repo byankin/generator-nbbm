57bfe32c76e4574cd5c6c1050fb9931cf77087f1
byankin <byankin@gmail.com>
2013-10-04 13:14:47 +0700

    index.html

--- app/index.js
+++ app/index.js
@@ -17,34 +17,51 @@ var NbbmGenerator = module.exports = function NbbmGenerator(args, options, confi
 util.inherits(NbbmGenerator, yeoman.generators.Base);
 
 NbbmGenerator.prototype.askFor = function askFor() {
-  var cb = this.async();
+ /* var cb = this.async();
 
   // have Yeoman greet the user.
-  console.log(this.yeoman);
-
+  //console.log(this.yeoman);
+/*
   var prompts = [{
-    type: 'confirm',
-    name: 'someOption',
-    message: 'Would you like to enable this option?',
-    default: true
+    name: 'appName',
+    message: 'What is your application name'
   }];
 
   this.prompt(prompts, function (props) {
-    this.someOption = props.someOption;
-
+    this.appName = props.appName;
+    console.log(this.appname);
     cb();
   }.bind(this));
+  */
 };
 
 NbbmGenerator.prototype.app = function app() {
-  this.mkdir('app');
-  this.mkdir('app/templates');
-
-  this.copy('_package.json', 'package.json');
+  
+  this.template('_package.json', 'package.json');
   this.copy('_bower.json', 'bower.json');
+  this.copy('bowerrc', '.bowerrc');
+  
+  this.mkdir('app/apps');
+  this.mkdir('app/less');
+  this.mkdir('app/css');
+  this.mkdir('app/templates');
+  this.mkdir('app/base');
+  this.mkdir('app/config');
+  //this.template('index.html','app/index.html');
+  this.indexFile = this.readFileAsString(path.join(this.sourceRoot(), 'index.html'));
+  this.indexFile = this.engine(this.indexFile, this);
+  var vendorJS = [
+    'bower_components/jquery/jquery.js',
+    'bower_components/underscore/underscore.js',
+    'bower_components/backbone/backbone.js',
+    'bower_components/backbone.marionette/lib/backbone.marionette.js'
+  ];
+  this.indexFile = this.appendScripts(this.indexFile, 'scripts/vendor.js', vendorJS);
+  this.write('app/index.html', this.indexFile);
+
 };
 
 NbbmGenerator.prototype.projectfiles = function projectfiles() {
-  this.copy('editorconfig', '.editorconfig');
-  this.copy('jshintrc', '.jshintrc');
+  //this.copy('editorconfig', '.editorconfig');
+  //this.copy('jshintrc', '.jshintrc');
 };
